0e4e7d56e6fb8c765f0388558616236a9907bf45
Daniel Faria <danielfaria18@gmail.com>
2013-04-01 21:08:36 -0300

    Before add i18n

---
+++ app/mailers/mailer.rb
@@ -0,0 +1,18 @@
+#class Devise::Mailer < Devise.parent_mailer.constantize
+  class Devise::Mailer < ::ActionMailer::Base
+    include Devise::Mailers::Helpers
+
+    # def confirmation_instructions(record, opts={})
+    def confirmation_instructions(record)
+      devise_mail(record, "#{:confirmation_instructions} teste #{record}", opts)
+    end
+
+    def reset_password_instructions(record)
+      puts record
+      devise_mail(record, :reset_password_instructions)
+    end
+
+    def unlock_instructions(record, opts={})
+      devise_mail(record, :unlock_instructions, opts)
+    end
+  end
